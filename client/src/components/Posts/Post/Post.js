475ba9e9043160fdb2c7000d107cbf1c264266ed
yangzhengca <yangzhengca>
2022-03-18 12:34:43 -0600

    added button tips for edit, comment and delete

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -10,39 +10,38 @@ import {
   ButtonBase,
 } from "@material-ui/core/";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
-import CommentIcon from '@material-ui/icons/Comment';
-import CommentOutlinedIcon from '@material-ui/icons/CommentOutlined';
+import CommentIcon from "@material-ui/icons/Comment";
+import CommentOutlinedIcon from "@material-ui/icons/CommentOutlined";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
 import moment from "moment";
 import { useDispatch } from "react-redux";
 import { deletePost, likePost } from "../../../actions/posts";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Tooltip from "@material-ui/core/Tooltip";
 
 const Post = ({ post, setCurrentId }) => {
   const user = JSON.parse(localStorage.getItem("profile"));
   const classes = useStyles();
   const dispatch = useDispatch();
   const history = useHistory();
-  const [ likes, setLikes ] = useState(post?.likes);
+  const [likes, setLikes] = useState(post?.likes);
 
   // use to simplify code
   const userId = user?.result.googleId || user?.result?._id;
   const hasLikedPost = post?.likes?.find((like) => like === userId);
-  
 
   const handleLike = async () => {
     dispatch(likePost(post._id));
 
-    if(hasLikedPost) {
-      setLikes(post.likes.filter((id) => id !== userId))
+    if (hasLikedPost) {
+      setLikes(post.likes.filter((id) => id !== userId));
     } else {
       setLikes([...post.likes, userId]);
     }
   };
-
-
 
   const Likes = () => {
     if (likes.length > 0) {
@@ -53,7 +52,6 @@ const Post = ({ post, setCurrentId }) => {
           {/* {likes.length > 2
             ? `You and ${likes.length - 1} others`
             : `${likes.length} like${likes.length > 1 ? "s" : ""}`} */}
-          
           {likes.length} {likes.length === 1 ? "Like" : "Likes"}
         </>
       ) : (
@@ -74,26 +72,36 @@ const Post = ({ post, setCurrentId }) => {
 
   // comments count component
   const CommentsCount = () => {
-    if (post.comments.length>0){
+    if (post.comments.length > 0) {
       return (
         <>
           <CommentIcon fontSize="small" />
           &nbsp;{post.comments.length}
         </>
-      )
+      );
     }
     return (
       <>
         <CommentOutlinedIcon fontSize="small" />
       </>
-    )
-  }
+    );
+  };
 
-  const openPost = () => history.push(`/posts/${post._id}`)
+  // button tips
+  const ButtonTooltip = withStyles((theme) => ({
+    tooltip: {
+      backgroundColor: theme.palette.common.white,
+      color: "rgba(0, 0, 0, 0.87)",
+      boxShadow: theme.shadows[1],
+      fontSize: 11,
+    },
+  }))(Tooltip);
+
+  const openPost = () => history.push(`/posts/${post._id}`);
 
   return (
     <Card className={classes.card} raised elevation={6}>
-      <ButtonBase 
+      <ButtonBase
         component="span"
         name="details"
         className={classes.cardAction}
@@ -113,21 +121,26 @@ const Post = ({ post, setCurrentId }) => {
             {moment(post.createdAt).fromNow()}
           </Typography>
         </div>
-        {(userId === post?.creator) && (
+
+        {/* edit button */}
+        {userId === post?.creator && (
           <div className={classes.overlay2}>
-            <Button
-              style={{ color: "white" }}
-              size="small"
-              // onClick={() => setCurrentId(post._id)}
-              onClick={(e) => {
-                e.stopPropagation();
-                setCurrentId(post._id);
-              }}
-            >
-              <MoreHorizIcon fontSize="default" />
-            </Button>
+            <ButtonTooltip title="Edit">
+              <Button
+                style={{ color: "white" }}
+                size="small"
+                // onClick={() => setCurrentId(post._id)}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setCurrentId(post._id);
+                }}
+              >
+                <MoreHorizIcon fontSize="default" />
+              </Button>
+            </ButtonTooltip>
           </div>
         )}
+
         <div className={classes.details}>
           <Typography variant="body2" color="textSecondary" component="h2">
             {post.tags.map((tag) => `#${tag} `)}
@@ -156,24 +169,31 @@ const Post = ({ post, setCurrentId }) => {
         >
           <Likes />
         </Button>
+
         {/* comments count button */}
-        <Button
-          size="small"
-          color="primary"
-          disabled={!user?.result}
-          onClick={openPost}
-        >
-          <CommentsCount />
-        </Button>
-        
-        {(userId === post?.creator) && (
+        <ButtonTooltip title="Comment">
           <Button
             size="small"
             color="primary"
-            onClick={() => dispatch(deletePost(post._id))}
+            disabled={!user?.result}
+            onClick={openPost}
           >
-            <DeleteIcon fontSize="small" /> Delete
+            <CommentsCount />
           </Button>
+        </ButtonTooltip>
+
+        {/* delete button */}
+        {userId === post?.creator && (
+          <ButtonTooltip title="Delete">
+            <Button
+              size="small"
+              color="primary"
+              onClick={() => dispatch(deletePost(post._id))}
+            >
+              <DeleteIcon fontSize="small" />
+               {/* Delete */}
+            </Button>
+          </ButtonTooltip>
         )}
       </CardActions>
     </Card>
